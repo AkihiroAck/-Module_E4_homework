9b3c844e601f9d89761a5a2019e8bf2c4cf089ee
AkihiroAck <106712263+AkihiroAck@users.noreply.github.com>
2024-10-19 18:40:02 +0400

    Update homework_5.js

--- homework_5.js
+++ homework_5.js
@@ -1,59 +1,33 @@
 // Задание: Переписать консольное приложение из предыдущего юнита на классы.
 
-// P.S.
-// Я не понял о каком консольном приложении именно говорилось, т.к. готового приложения особо не было. 
-// Кодом экспериментировалось до конца вебинара.
-// Я написал похожий код с похожими функциями конструкторами и перевел их в класс ниже, после знаков "-----".
-// Надеюсь на понимание.
-
-
-function Animal(name) {
-  this.name = name;
+class Parent {
+  constructor(ownCity) {
+    this.ownCity = ownCity || 'Moscow';
+    this.hasFlat = true;
+  }
+  getInfo() {
+    return 'I live in ' + this.ownCity;
+  }
 }
 
-Animal.prototype.speak = function () {
-  console.log(this.name + ' издает звук.');
-};
-
-function Dog(name, breed) {
-  Animal.call(this, name);
-  this.breed = breed;
+class Child extends Parent {
+  constructor(isStudent, city, ownCity) {
+    super(ownCity);
+    this.isStudent = isStudent;
+    this.city = this.isStudent ? city : this.ownCity;
+  }
+  getInfo() {
+    if (this.isStudent) {
+      return 'I study in ' + this.city;
+    } else {
+      return super.getInfo();
+    }
+  }
 }
 
-Dog.prototype = Object.create(Animal.prototype);
+const student = new Child(true, 'Piter');
+console.log(student.getInfo());
 
-Dog.prototype.speak = function () {
-  console.log(this.name + ' гавкает!');
-};
-
-const myDog1 = new Dog('Бобик', 'Терьер');
-myDog1.speak(); // Вывод: Бобик гавкает!
-
-
-// -------------------------------------------------------------
-
-
-class AnimalClass {
-  constructor(name) {
-    this.name = name;
-  };
-
-  speak() {
-    console.log(this.name + ' издает звук.');
-  };
-};
-
-class DogClass extends AnimalClass {
-  constructor(name, breed) {
-    super(name);
-    this.breed = breed;
-  };
-
-  speak() {
-    console.log(this.name + ' гавкает!');
-  };
-};
-
-
-const myDog2 = new DogClass('Бобик', 'Терьер');
-myDog2.speak() // Вывод: Бобик гавкает!
+const employee = new Child(false, 'New York');
+console.log(employee.getInfo());
+console.log(employee.city);
